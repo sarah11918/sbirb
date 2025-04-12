85829cee30db242ce378161ada5b6ae6de20d31f
sarahrainsberger <sarahrainsberger@gmail.com>
2025-04-12 15:51:55 +0000

    update recent bird list to show full date including year

--- src/bird/Recent.jsx
+++ src/bird/Recent.jsx
@@ -84,7 +84,7 @@ export default function Recent() {
      
       <h3>Reported in the last 14 days...</h3>
       {recentBirds.map((bird) => (
-        <p><a href={`https://www.ebird.org/species/${bird.speciesCode}`}>{bird.comName}</a> on {bird.obsDt.toString().slice(5,10)}</p>
+        <p><a href={`https://www.ebird.org/species/${bird.speciesCode}`}>{bird.comName}</a> on {bird.obsDt.toString().slice(0,10)}</p>
       ))}
     </div>
   );
